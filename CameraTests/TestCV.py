6adfec037d43406a155239c23b279618f6bea905
Amice <amice@seas.upenn.edu>
2018-11-08 21:47:44 -0500

    Communication completion

--- CameraTests/TestCV.py
+++ CameraTests/TestCV.py
@@ -13,7 +13,7 @@ camera.resolution = (16*photoHeight/9, photoHeight)
 # captue an image and read it back in
 # (Do this because picamera does not play nice with openCV?)
 #
-camera.capture('Blah.jpg')
+camera.capture('TestAligned.jpg')
 '''
 imgColor = cv2.imread('blackRoad.jpg') 
 #
