8d7003736c1db9a342e455bb93ea062b90dea7e9
AlexandreAmice <amice@seas.upenn.edu>
2018-12-25 16:12:33 -0500

    Added Arduino Code

--- CameraTests/TestPy.py
+++ CameraTests/TestPy.py
@@ -116,16 +116,3 @@ sideBySide(image, temp, "HSV.jpg")
 
 
 
-
-# low_black = np.array([50, 50, 50]) #if value is
-# high_black = np.array([100, 255, 255])
-# mask = cv2.inRange(img, low_black, high_black)
-# cv2.imwrite('mask.jpg', mask)
-# blurMask = cv2.GaussianBlur(mask, (7,7), 3)
-# cv2.imwrite('blurMask.jpg', blurMask)
-# cropped = mask[300:,500:]
-# cv2.imwrite('maskCrop.jpg', cropped)
-# cv2.imwrite('cannyCrop.jpg', cv2.Canny(cropped, 80,20))
-# #maskRGB = cv2.cvtColor(mask, cv2.COLOR_HSV2RGB)
-# #cv2.imwrite('maskRGB.jpg', maskRGB)
-
